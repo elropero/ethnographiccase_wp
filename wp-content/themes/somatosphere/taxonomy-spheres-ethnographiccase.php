11be5cb61e5a3ed2928189d386a971488d2e2ab8
Andrew Roper <andrewroper@gmail.com>
2015-05-28 16:15:40 +0200

    More cleanup

--- wp-content/themes/somatosphere/taxonomy-spheres-ethnographiccase.php
+++ wp-content/themes/somatosphere/taxonomy-spheres-ethnographiccase.php
@@ -198,65 +198,7 @@ get_header(); ?>
 </svg> 
 
 
-<div id="content"> <!-- NOTE: This gives the content bg -->
-
-
-<?php /* if (is_paged()) { ?>
-				<?php get_template_part( 'loop', 'index' ); ?>
-			
-<?php } else { ?>
-<?php 
-$pid = 1;
-$pid1 = 1;
-global $query_string;
-global $wp_query;
-?>
-								<?php include 'sphereloop.php' ?>
-<?php /* 		
-				<div id="frontbig">
-<?php $sticky = get_option( 'sticky_posts' );
-if ( $sticky[0] ) {
-$args = array_merge( $wp_query->query, array(
-	'posts_per_page' => 1,
-	'post__in'  => $sticky,
-	'ignore_sticky_posts' => 1,
-	'post_type' => 'post'
-	)
-);
-query_posts( $args );
-//$catquery = new WP_Query( $args );
-} else {
-
-?>
-
-
-					<?php query_posts( $query_string . '&posts_per_page=1&post_type=post&cat=1,276,295'); } ?>
-					<?php // $catquery = new WP_Query('posts_per_page=1&cat=11'); ?>
-					<?php $my_count = 0 ?>
-					<?php include 'sphereloop.php' ?>
-					<?php $pid1 = $pid; ?>
-				</div>
-				<div id="frontsmall">
-					<?php $args = array_merge( $wp_query->query, array(
-						'posts_per_page' => 6,
-						'cat' => '-252',
-						'post__not_in'  => array($pid1),
-						'post_type' => 'post',
-						'paged' => $paged
-						)
-					); ?>
-					<div class="frontsmall left">
-						<?php query_posts( $args ); ?>
-						<?php $my_count = 0 ?>
-						<?php include 'sphereloop.php' ?>
-					</div>
-					<div class="frontsmall right">
-						<?php query_posts( $args ); ?>
-						<?php $my_count = 1 ?>
-						<?php include 'sphereloop.php' ?>
-					</div>
-				</div>
-*/ ?>
+    <div id="content"> <!-- NOTE: This gives the content bg -->
 
 	<?php if (  $wp_query->max_num_pages > 1 ) : ?>
 		<nav class="nav-below" role="navigation">
@@ -268,8 +210,8 @@ query_posts( $args );
 	<?php // } ?>
 
 
-			</div><!-- #content -->
-		</div><!-- #primary -->
+    </div><!-- #content -->
+</div><!-- #primary -->
 
-<?php get_sidebar('commonplaces'); ?>
+<?php get_sidebar('ethnographiccase'); ?>
 <?php get_footer(); ?>
