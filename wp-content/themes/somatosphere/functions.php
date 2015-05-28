4257878f4ee6e4213cf3c953245fc3b1e7549989
Andrew Roper <andrewroper@gmail.com>
2015-05-28 16:02:02 +0200

    Fix rewrite rules and normalize sphere name

--- wp-content/themes/somatosphere/functions.php
+++ wp-content/themes/somatosphere/functions.php
@@ -379,11 +379,11 @@ function sphere_rewrite_rules( $wp_rewrite )
 		'commonplaces/page/(.+)' => 'index.php?spheres=commonplaces&paged=' .$wp_rewrite->preg_index(1),
 		'commonplaces/(.+)' => 'index.php?spheres=commonplaces&category_name=' .$wp_rewrite->preg_index(1),
 		'commonplaces' => 'index.php?spheres=commonplaces',
-        's/case/page/(.+)' => 'index.php?spheres=ethnographiccase&paged=' .$wp_rewrite->preg_index(1),
+        's/ethnographiccase/page/(.+)' => 'index.php?spheres=ethnographiccase&paged=' .$wp_rewrite->preg_index(1),
 		's/(.+)/(.+)' => 'index.php?spheres=' . $wp_rewrite->preg_index(1) . '&category_name=' .$wp_rewrite->preg_index(2),
-		'case/page/(.+)' => 'index.php?spheres=ethnographiccase&paged=' .$wp_rewrite->preg_index(1),
-		'case/(.+)' => 'index.php?spheres=ethnographiccase&category_name=' .$wp_rewrite->preg_index(1),
-		'case' => 'index.php?spheres=ethnographiccase'
+		'ethnographiccase/page/(.+)' => 'index.php?spheres=ethnographiccase&paged=' .$wp_rewrite->preg_index(1),
+		'ethnographiccase/(.+)' => 'index.php?spheres=ethnographiccase&category_name=' .$wp_rewrite->preg_index(1),
+		'ethnographiccase' => 'index.php?spheres=ethnographiccase'
 	);
 	$wp_rewrite->rules = $new_rules + $wp_rewrite->rules;
 }
